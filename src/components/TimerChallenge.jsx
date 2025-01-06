41636389986a42c3156d06a42a622a36ddfe3424
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-06 15:40:39 +0530

    changes for the useRef- those vale that doesn't maintain the state directly but want to use it in background

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,19 +1,36 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 
 const TimerChallenge = ({title, targetTime}) => {
+    const timer = useRef(null);
+    const [timerStarted, setTimerStarted] = useState(false)
+    const [timerExpired, setTimerExpired] = useState(false);
+
+    
+    const handleStart  = () => {
+        timer.current = setTimeout(() => {
+            setTimerExpired(true)
+        }, targetTime*1000);
+        setTimerStarted(true);
+    }
+
+    const handleStop = () => {
+        clearTimeout(timer.current);
+    }
+
     return (
         <section className='challenge'>
             <h2>{title}</h2>
+            {timerExpired && (<p>You Lost!</p>)}
             <p className="challenge-time">
                 {targetTime}  second{targetTime > 1 ? 's' : ''}
             </p>
             <p>
-                <button>
-                    Start Challenge
+                <button onClick={timerStarted ? handleStop : handleStart}>
+                    {timerStarted ? "Stop" : "Start"} Challenge
                 </button>
             </p>
-            <p className=''>
-                Time is running ..../ Timer Inactive
+            <p className={timerStarted ? 'active' : undefined}>
+                {timerStarted ? "Time is running ...." :  "Timer Inactive"}
             </p>
         </section>
     );
