2720e1be92e18b55e7fe5eedfa0ff2d34fc4805f
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-06 11:13:04 +0530

    changes for the file with useState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,31 @@
+import React, {useState, useRef} from "react";
+
 export default function Player() {
+  const playerName = useRef();
+
+  const [enteredPlayerName, setEnteredPlayerName]= useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleChange = (event) => {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  const handleSubmit = () => {
+    //setSubmitted(true);
+    console.log(playerName.current.value) ;
+    setEnteredPlayerName(playerName.current.value);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" 
+          placeholder="Enter your name"
+          value={enteredPlayerName}
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
