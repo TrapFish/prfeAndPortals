1182fec2c2c52d555f6969e706b2727b24e4e349
TrapFish <92753280+TrapFish@users.noreply.github.com>
2025-01-06 11:25:41 +0530

    removed useState and used useRef

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,26 +4,26 @@ export default function Player() {
   const playerName = useRef();
 
   const [enteredPlayerName, setEnteredPlayerName]= useState('');
-  const [submitted, setSubmitted] = useState(false);
+  // const [submitted, setSubmitted] = useState(false);
 
-  const handleChange = (event) => {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
+  // const handleChange = (event) => {
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(event.target.value);
+  // }
 
   const handleSubmit = () => {
     //setSubmitted(true);
-    console.log(playerName.current.value) ;
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
   }
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome { enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
         <input ref={playerName} type="text" 
           placeholder="Enter your name"
-          value={enteredPlayerName}
-          onChange={handleChange}
+          // value={enteredPlayerName}
+          // onChange={handleChange}
         />
         <button onClick={handleSubmit}>Set Name</button>
       </p>
